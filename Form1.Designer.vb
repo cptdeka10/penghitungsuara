57fb28985ae9368dbca87a63d16dfe81b9f4eb01
Prajna Prasetya <31295104+cptdeka10@users.noreply.github.com>
2018-05-21 17:14:41 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,382 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formPenghitungSuara
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPersen1 = New System.Windows.Forms.Label()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lblSuara1 = New System.Windows.Forms.Label()
+        Me.lblSuara2 = New System.Windows.Forms.Label()
+        Me.lblSuara3 = New System.Windows.Forms.Label()
+        Me.btnTidakSah = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.lblSuaraMasuk = New System.Windows.Forms.Label()
+        Me.lblSuaraSah = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.lblPersen2 = New System.Windows.Forms.Label()
+        Me.lblPersen3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblSuaraTidakSah = New System.Windows.Forms.Label()
+        Me.btnMin1 = New System.Windows.Forms.Button()
+        Me.btnMin2 = New System.Windows.Forms.Button()
+        Me.btnMin3 = New System.Windows.Forms.Button()
+        Me.btnMinTidakSah = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblPersen1
+        '
+        Me.lblPersen1.AutoSize = True
+        Me.lblPersen1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPersen1.Location = New System.Drawing.Point(191, 95)
+        Me.lblPersen1.Name = "lblPersen1"
+        Me.lblPersen1.Size = New System.Drawing.Size(220, 73)
+        Me.lblPersen1.TabIndex = 0
+        Me.lblPersen1.Text = "0.00%"
+        Me.lblPersen1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(253, 335)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 61)
+        Me.btn1.TabIndex = 3
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(593, 335)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(75, 61)
+        Me.btn2.TabIndex = 4
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(994, 335)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(75, 61)
+        Me.btn3.TabIndex = 5
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(87, 229)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 24)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "NAMA"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(87, 293)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(79, 24)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "SUARA"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(204, 226)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(179, 29)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblSuara1
+        '
+        Me.lblSuara1.AutoSize = True
+        Me.lblSuara1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSuara1.Location = New System.Drawing.Point(281, 293)
+        Me.lblSuara1.Name = "lblSuara1"
+        Me.lblSuara1.Size = New System.Drawing.Size(21, 24)
+        Me.lblSuara1.TabIndex = 11
+        Me.lblSuara1.Text = "0"
+        Me.lblSuara1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSuara2
+        '
+        Me.lblSuara2.AutoSize = True
+        Me.lblSuara2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSuara2.Location = New System.Drawing.Point(619, 293)
+        Me.lblSuara2.Name = "lblSuara2"
+        Me.lblSuara2.Size = New System.Drawing.Size(21, 24)
+        Me.lblSuara2.TabIndex = 12
+        Me.lblSuara2.Text = "0"
+        Me.lblSuara2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSuara3
+        '
+        Me.lblSuara3.AutoSize = True
+        Me.lblSuara3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSuara3.Location = New System.Drawing.Point(1019, 293)
+        Me.lblSuara3.Name = "lblSuara3"
+        Me.lblSuara3.Size = New System.Drawing.Size(21, 24)
+        Me.lblSuara3.TabIndex = 13
+        Me.lblSuara3.Text = "0"
+        Me.lblSuara3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnTidakSah
+        '
+        Me.btnTidakSah.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTidakSah.Location = New System.Drawing.Point(489, 479)
+        Me.btnTidakSah.Name = "btnTidakSah"
+        Me.btnTidakSah.Size = New System.Drawing.Size(75, 55)
+        Me.btnTidakSah.TabIndex = 14
+        Me.btnTidakSah.Text = "TIDAK SAH"
+        Me.btnTidakSah.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(691, 470)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(156, 24)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "SUARA MASUK"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(691, 509)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(127, 24)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "SUARA SAH"
+        '
+        'lblSuaraMasuk
+        '
+        Me.lblSuaraMasuk.AutoSize = True
+        Me.lblSuaraMasuk.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSuaraMasuk.Location = New System.Drawing.Point(912, 470)
+        Me.lblSuaraMasuk.Name = "lblSuaraMasuk"
+        Me.lblSuaraMasuk.Size = New System.Drawing.Size(21, 24)
+        Me.lblSuaraMasuk.TabIndex = 17
+        Me.lblSuaraMasuk.Text = "0"
+        Me.lblSuaraMasuk.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSuaraSah
+        '
+        Me.lblSuaraSah.AutoSize = True
+        Me.lblSuaraSah.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSuaraSah.Location = New System.Drawing.Point(912, 510)
+        Me.lblSuaraSah.Name = "lblSuaraSah"
+        Me.lblSuaraSah.Size = New System.Drawing.Size(21, 24)
+        Me.lblSuaraSah.TabIndex = 18
+        Me.lblSuaraSah.Text = "0"
+        Me.lblSuaraSah.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(537, 226)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(179, 29)
+        Me.TextBox2.TabIndex = 19
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(941, 226)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(179, 29)
+        Me.TextBox3.TabIndex = 20
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnReset
+        '
+        Me.btnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.Location = New System.Drawing.Point(1117, 558)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 21
+        Me.btnReset.Text = "RESET"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'lblPersen2
+        '
+        Me.lblPersen2.AutoSize = True
+        Me.lblPersen2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPersen2.Location = New System.Drawing.Point(524, 95)
+        Me.lblPersen2.Name = "lblPersen2"
+        Me.lblPersen2.Size = New System.Drawing.Size(220, 73)
+        Me.lblPersen2.TabIndex = 22
+        Me.lblPersen2.Text = "0.00%"
+        Me.lblPersen2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblPersen3
+        '
+        Me.lblPersen3.AutoSize = True
+        Me.lblPersen3.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPersen3.Location = New System.Drawing.Point(928, 95)
+        Me.lblPersen3.Name = "lblPersen3"
+        Me.lblPersen3.Size = New System.Drawing.Size(220, 73)
+        Me.lblPersen3.TabIndex = 23
+        Me.lblPersen3.Text = "0.00%"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(691, 549)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(192, 24)
+        Me.Label1.TabIndex = 24
+        Me.Label1.Text = "SUARA TIDAK SAH"
+        '
+        'lblSuaraTidakSah
+        '
+        Me.lblSuaraTidakSah.AutoSize = True
+        Me.lblSuaraTidakSah.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSuaraTidakSah.Location = New System.Drawing.Point(912, 549)
+        Me.lblSuaraTidakSah.Name = "lblSuaraTidakSah"
+        Me.lblSuaraTidakSah.Size = New System.Drawing.Size(21, 24)
+        Me.lblSuaraTidakSah.TabIndex = 25
+        Me.lblSuaraTidakSah.Text = "0"
+        Me.lblSuaraTidakSah.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnMin1
+        '
+        Me.btnMin1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMin1.Location = New System.Drawing.Point(253, 402)
+        Me.btnMin1.Name = "btnMin1"
+        Me.btnMin1.Size = New System.Drawing.Size(75, 30)
+        Me.btnMin1.TabIndex = 26
+        Me.btnMin1.Text = "-"
+        Me.btnMin1.UseVisualStyleBackColor = True
+        '
+        'btnMin2
+        '
+        Me.btnMin2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMin2.Location = New System.Drawing.Point(593, 402)
+        Me.btnMin2.Name = "btnMin2"
+        Me.btnMin2.Size = New System.Drawing.Size(75, 30)
+        Me.btnMin2.TabIndex = 27
+        Me.btnMin2.Text = "-"
+        Me.btnMin2.UseVisualStyleBackColor = True
+        '
+        'btnMin3
+        '
+        Me.btnMin3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMin3.Location = New System.Drawing.Point(994, 402)
+        Me.btnMin3.Name = "btnMin3"
+        Me.btnMin3.Size = New System.Drawing.Size(75, 30)
+        Me.btnMin3.TabIndex = 28
+        Me.btnMin3.Text = "-"
+        Me.btnMin3.UseVisualStyleBackColor = True
+        '
+        'btnMinTidakSah
+        '
+        Me.btnMinTidakSah.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMinTidakSah.Location = New System.Drawing.Point(489, 543)
+        Me.btnMinTidakSah.Name = "btnMinTidakSah"
+        Me.btnMinTidakSah.Size = New System.Drawing.Size(75, 30)
+        Me.btnMinTidakSah.TabIndex = 29
+        Me.btnMinTidakSah.Text = "-"
+        Me.btnMinTidakSah.UseVisualStyleBackColor = True
+        '
+        'formPenghitungSuara
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1237, 593)
+        Me.Controls.Add(Me.btnMinTidakSah)
+        Me.Controls.Add(Me.btnMin3)
+        Me.Controls.Add(Me.btnMin2)
+        Me.Controls.Add(Me.btnMin1)
+        Me.Controls.Add(Me.lblSuaraTidakSah)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblPersen3)
+        Me.Controls.Add(Me.lblPersen2)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.lblSuaraSah)
+        Me.Controls.Add(Me.lblSuaraMasuk)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.btnTidakSah)
+        Me.Controls.Add(Me.lblSuara3)
+        Me.Controls.Add(Me.lblSuara2)
+        Me.Controls.Add(Me.lblSuara1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.lblPersen1)
+        Me.Name = "formPenghitungSuara"
+        Me.Text = "PENGHITUNG SUARA"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblPersen1 As System.Windows.Forms.Label
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents lblSuara1 As System.Windows.Forms.Label
+    Friend WithEvents lblSuara2 As System.Windows.Forms.Label
+    Friend WithEvents lblSuara3 As System.Windows.Forms.Label
+    Friend WithEvents btnTidakSah As System.Windows.Forms.Button
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents lblSuaraMasuk As System.Windows.Forms.Label
+    Friend WithEvents lblSuaraSah As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents btnReset As System.Windows.Forms.Button
+    Friend WithEvents lblPersen2 As System.Windows.Forms.Label
+    Friend WithEvents lblPersen3 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblSuaraTidakSah As System.Windows.Forms.Label
+    Friend WithEvents btnMin1 As System.Windows.Forms.Button
+    Friend WithEvents btnMin2 As System.Windows.Forms.Button
+    Friend WithEvents btnMin3 As System.Windows.Forms.Button
+    Friend WithEvents btnMinTidakSah As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,205 @@
+﻿Public Class formPenghitungSuara
+    Private Sub btnReset_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles btnReset.Click
+        lblPersen1.Text = "0.00%"
+        lblPersen2.Text = "0.00%"
+        lblPersen3.Text = "0.00%"
+        lblSuara1.Text = "0"
+        lblSuara2.Text = "0"
+        lblSuara3.Text = "0"
+        lblSuaraMasuk.Text = "0"
+        lblSuaraSah.Text = "0"
+        lblSuaraTidakSah.Text = "0"
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+
+    Private Sub btn1_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles btn1.Click
+        Dim a As Decimal
+        a = lblSuaraMasuk.Text
+        a = a + 1
+        lblSuaraMasuk.Text = a.ToString
+        a = lblSuaraSah.Text
+        a = a + 1
+        lblSuaraSah.Text = a.ToString
+
+        a = lblSuara1.Text
+        a = a + 1
+        lblSuara1.Text = a.ToString
+
+        Dim b As Double
+        Dim c As Double
+        Dim d As Double
+        c = lblSuaraSah.Text
+        b = lblSuara1.Text
+        d = b * 100 / c
+        lblPersen1.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara2.Text
+        d = b * 100 / c
+        lblPersen2.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara3.Text
+        d = b * 100 / c
+        lblPersen3.Text = String.Format("{0:F2}", d) + "%"
+    End Sub
+
+    Private Sub btn2_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles btn2.Click
+        Dim a As Decimal
+        a = lblSuaraMasuk.Text
+        a = a + 1
+        lblSuaraMasuk.Text = a.ToString
+        a = lblSuaraSah.Text
+        a = a + 1
+        lblSuaraSah.Text = a.ToString
+
+        a = lblSuara2.Text
+        a = a + 1
+        lblSuara2.Text = a.ToString
+
+        Dim b As Double
+        Dim c As Double
+        Dim d As Double
+        c = lblSuaraSah.Text
+        b = lblSuara1.Text
+        d = b * 100 / c
+        lblPersen1.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara2.Text
+        d = b * 100 / c
+        lblPersen2.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara3.Text
+        d = b * 100 / c
+        lblPersen3.Text = String.Format("{0:F2}", d) + "%"
+    End Sub
+
+    Private Sub btn3_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles btn3.Click
+        Dim a As Decimal
+        a = lblSuaraMasuk.Text
+        a = a + 1
+        lblSuaraMasuk.Text = a.ToString
+        a = lblSuaraSah.Text
+        a = a + 1
+        lblSuaraSah.Text = a.ToString
+
+        a = lblSuara3.Text
+        a = a + 1
+        lblSuara3.Text = a.ToString
+
+        Dim b As Double
+        Dim c As Double
+        Dim d As Double
+        c = lblSuaraSah.Text
+        b = lblSuara1.Text
+        d = b * 100 / c
+        lblPersen1.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara2.Text
+        d = b * 100 / c
+        lblPersen2.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara3.Text
+        d = b * 100 / c
+        lblPersen3.Text = String.Format("{0:F2}", d) + "%"
+    End Sub
+
+    Private Sub btnTidakSah_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles btnTidakSah.Click
+        Dim a As Decimal
+        a = lblSuaraTidakSah.Text
+        a = a + 1
+        lblSuaraTidakSah.Text = a.ToString
+        a = lblSuaraMasuk.Text
+        a = a + 1
+        lblSuaraMasuk.Text = a.ToString
+    End Sub
+
+    Private Sub btnMin1_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles btnMin1.Click
+        Dim a As Decimal
+        a = lblSuaraMasuk.Text
+        a = a - 1
+        lblSuaraMasuk.Text = a.ToString
+        a = lblSuaraSah.Text
+        a = a - 1
+        lblSuaraSah.Text = a.ToString
+
+        a = lblSuara1.Text
+        a = a - 1
+        lblSuara1.Text = a.ToString
+
+        Dim b As Double
+        Dim c As Double
+        Dim d As Double
+        c = lblSuaraSah.Text
+        b = lblSuara1.Text
+        d = b * 100 / c
+        lblPersen1.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara2.Text
+        d = b * 100 / c
+        lblPersen2.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara3.Text
+        d = b * 100 / c
+        lblPersen3.Text = String.Format("{0:F2}", d) + "%"
+    End Sub
+
+    Private Sub btnMin2_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles btnMin2.Click
+        Dim a As Decimal
+        a = lblSuaraMasuk.Text
+        a = a - 1
+        lblSuaraMasuk.Text = a.ToString
+        a = lblSuaraSah.Text
+        a = a - 1
+        lblSuaraSah.Text = a.ToString
+
+        a = lblSuara2.Text
+        a = a - 1
+        lblSuara2.Text = a.ToString
+
+        Dim b As Double
+        Dim c As Double
+        Dim d As Double
+        c = lblSuaraSah.Text
+        b = lblSuara1.Text
+        d = b * 100 / c
+        lblPersen1.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara2.Text
+        d = b * 100 / c
+        lblPersen2.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara3.Text
+        d = b * 100 / c
+        lblPersen3.Text = String.Format("{0:F2}", d) + "%"
+    End Sub
+
+    Private Sub btnMin3_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles btnMin3.Click
+        Dim a As Decimal
+        a = lblSuaraMasuk.Text
+        a = a - 1
+        lblSuaraMasuk.Text = a.ToString
+        a = lblSuaraSah.Text
+        a = a - 1
+        lblSuaraSah.Text = a.ToString
+
+        a = lblSuara3.Text
+        a = a - 1
+        lblSuara3.Text = a.ToString
+
+        Dim b As Double
+        Dim c As Double
+        Dim d As Double
+        c = lblSuaraSah.Text
+        b = lblSuara1.Text
+        d = b * 100 / c
+        lblPersen1.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara2.Text
+        d = b * 100 / c
+        lblPersen2.Text = String.Format("{0:F2}", d) + "%"
+        b = lblSuara3.Text
+        d = b * 100 / c
+        lblPersen3.Text = String.Format("{0:F2}", d) + "%"
+    End Sub
+
+    Private Sub btnMinTidakSah_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles btnMinTidakSah.Click
+        Dim a As Decimal
+        a = lblSuaraTidakSah.Text
+        a = a - 1
+        lblSuaraTidakSah.Text = a.ToString
+        a = lblSuaraMasuk.Text
+        a = a - 1
+        lblSuaraMasuk.Text = a.ToString
+    End Sub
+
+End Class
